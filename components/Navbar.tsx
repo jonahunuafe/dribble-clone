3273e253ac43ee97d53eb5a71ac70de10602c468
Jonah Unuafe <fejisfejiro@gmail.com>
2024-09-02 17:13:20 +0100

    created a constanf folder to store links

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,8 +1,24 @@
+import Link from 'next/link';
+import Image from 'next/image';
 import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav className='flexBetween navbar'>
+      <div className='flex-1 flexStart gap-10'>
+        <Link href='/'>
+          <Image
+            alt='logo' 
+            src='/logo.svg'
+            width={115}
+            height={43}
+          />
+        </Link>
+        <ul className='xl:flex hidden text-small gap-7 '>
+          
+        </ul>
+      </div>
+    </nav>
   )
 }
 
