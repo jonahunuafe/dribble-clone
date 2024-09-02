b8d215360599c4ab99e21f33fb56c640fae3c4ae
Jonah Unuafe <fejisfejiro@gmail.com>
2024-09-02 07:16:21 +0100

    created custom hook

--- app/AppContext.tsx
+++ app/AppContext.tsx
@@ -5,8 +5,8 @@ import React, { createContext, useState, ReactNode, useContext } from "react";
 // Define the shape of the context state
 interface AppState {
     openExpenseWindowObject: {
-        openExpensewindow: boolean;
-        setOpenExpensewindow: React.Dispatch<React.SetStateAction<boolean>>;
+        openExpenseWindow: boolean;
+        setOpenExpenseWindow: React.Dispatch<React.SetStateAction<boolean>>;
     }
 }
 
@@ -14,7 +14,30 @@ interface AppState {
 
 const defaultState: AppState = {
     openExpenseWindowObject: {
-        openExpensewindow: false,
-        setOpenExpensewindow: () => {},
+        openExpenseWindow: false,
+        setOpenExpenseWindow: () => {},
     }
 }
+
+
+// create the context with the default values
+const AppContext = createContext<AppState>(defaultState);
+
+// Create a provider comnponent
+export const AppProvider: React.FC<{ children: ReactNode }> = ({
+    children,
+}) => {
+    const [openExpenseWindow, setOpenExpenseWindow] = useState(false)
+    return (
+        <AppContext.Provider 
+            value={{ 
+                openExpenseWindowObject: { openExpenseWindow, setOpenExpenseWindow },
+            }}
+        >
+            children
+        </AppContext.Provider>
+    )
+}
+
+// Custom hook to use the AppContext
+export const useAppContext = () => useContext(AppContext);
