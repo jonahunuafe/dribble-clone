5ebe9273167d8a1f63960bc8ef6a58a9cb4ad2cc
Jonah Unuafe <fejisfejiro@gmail.com>
2024-09-03 08:50:49 +0100

    created FooterColumn component

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,6 +1,15 @@
 import React from 'react'
 import Image from 'next/image';
 
+const FooterColumn = () => (
+  <div className='footer_column'>
+    <h4 className='font-semibold'>Title</h4>
+    <ul className='flex flex-col gap-2 font-normal'>
+      Links
+    </ul>
+  </div>
+)
+
 const Footer = () => {
   return (
     <footer className='flexStart footer'>
@@ -16,6 +25,10 @@ const Footer = () => {
           <p className='text-start text-sm font-normal mt-5 max-w-xs'>
             Flexibble is the world&apos;s leading community for creatives to share, grow, and get hired.
           </p>
+        </div>
+
+        <div className='flex flex-wrap gap-12'>
+          <FooterColumn />
         </div>
       </div>
     </footer>
