2633f1dc53ed3164e9106a31011aadc7b1339db1
Jonah Unuafe <fejisfejiro@gmail.com>
2024-09-03 09:52:09 +0100

    populating the footer with links

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,11 +1,22 @@
 import React from 'react'
+import { footerLinks } from '@/constants';
 import Image from 'next/image';
+import Link from 'next/link';
 
-const FooterColumn = () => (
+type ColumnProps = {
+  title: string;
+  links: Array<string>;
+}
+
+const FooterColumn = ({ title, links }: ColumnProps) => (
   <div className='footer_column'>
-    <h4 className='font-semibold'>Title</h4>
+    <h4 className='font-semibold'>{title}</h4>
     <ul className='flex flex-col gap-2 font-normal'>
-      Links
+      {links.map((link) => (
+        <Link href='/' key={link}>
+          {link}
+        </Link>
+      ))}
     </ul>
   </div>
 )
@@ -28,7 +39,40 @@ const Footer = () => {
         </div>
 
         <div className='flex flex-wrap gap-12'>
-          <FooterColumn />
+          <FooterColumn 
+            title={footerLinks[0].title}
+            links={footerLinks[0].links}
+          />
+          <div className='flex flex-1 flex-col gap-4'>
+            <FooterColumn 
+              title={footerLinks[1].title}
+              links={footerLinks[1].links}
+            />
+            <FooterColumn 
+              title={footerLinks[2].title}
+              links={footerLinks[2].links}
+            />
+          </div>
+
+          <FooterColumn 
+            title={footerLinks[3].title}
+            links={footerLinks[3].links}
+          />
+          <div className='flex flex-1 flex-col gap-4'>
+            <FooterColumn 
+              title={footerLinks[4].title}
+              links={footerLinks[4].links}
+            />
+            <FooterColumn 
+              title={footerLinks[5].title}
+              links={footerLinks[5].links}
+            />
+          </div>
+
+          <FooterColumn 
+            title={footerLinks[6].title}
+            links={footerLinks[6].links}
+          />
         </div>
       </div>
     </footer>
