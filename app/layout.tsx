f64eafdc79d610f7458e871d2615b526990e019d
Jonah Unuafe <fejisfejiro@gmail.com>
2024-09-05 07:27:56 +0100

    removed Navbar and Footer from Layout component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,7 @@
-import "./globals.css";
-import Navbar from "@/components/Navbar";
-import Footer from "@/components/Footer";
 
 export const metadata = {
-  title: "Flexibble",
-  description: "Showcase and discover remarkable developer projects",
+  title: "",
+  description: "",
 };
 
 export default function RootLayout({
@@ -15,11 +12,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        <Navbar />
         <main>
           {children}
         </main>
-        <Footer />
       </body>
     </html>
   );
