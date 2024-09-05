072c32b4cfb36fb734921c962d67d8884dfd7f37
Jonah Unuafe <fejisfejiro@gmail.com>
2024-09-05 08:38:06 +0100

    added utils.ts file in lib folder

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,18 @@
+import { Metadata } from "next";
+import { Plus_Jakarta_Sans } from "next/font/google";
+// import "./globals.css"
 
-export const metadata = {
-  title: "",
-  description: "",
+import { cn } from "@/lib/utils";
+
+const fontSans = Plus_Jakarta_Sans({ 
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+  variable: "--font-sans"
+})
+
+export const metadata: Metadata = {
+  title: "CarePulse",
+  description: "A healthcare management system",
 };
 
 export default function RootLayout({
@@ -11,7 +22,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>
+      <body className={cn("min-h-screen bg-dark-300 font-sans antialiased", fontSans.variable)}>
         <main>
           {children}
         </main>
--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,9 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <section>
-    </section>
+    <div>
+      <h1>Home</h1>
+    </div>
   )
 }
 
