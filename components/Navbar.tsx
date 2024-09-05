14cbf966ae28eb120cd19014e81c6b37cf1ec7f5
Jonah Unuafe <fejisfejiro@gmail.com>
2024-09-05 07:23:33 +0100

    removed the contents of Navbar component

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,44 +1,8 @@
-import Link from 'next/link';
-import Image from 'next/image';
-import { NavLinks } from '@/constants';
-import React from 'react'
-import AuthProviders from './AuthProviders';
 
 const Navbar = () => {
-  const session = {}
+
   return (
-    <nav className='flexBetween navbar'>
-      <div className='flex-1 flexStart gap-10'>
-        <Link href='/'>
-          <Image
-            alt='logo' 
-            src='/logo.svg'
-            width={115}
-            height={43}
-          />
-        </Link>
-        <ul className='xl:flex hidden text-small gap-7 '>
-          {NavLinks.map((link) => (
-            <Link href={link.href} key={link.key}>
-              {link.text}
-            </Link>
-          ))}
-        </ul>
-      </div>
-
-      <div className='flexCenter gap-4'>
-        {session ? (
-          <>
-            UserPhoto
-
-            <Link href='/create-project'>
-              Share your work
-            </Link>
-          </>
-        ) : (
-          <AuthProviders />
-        )}
-      </div>
+    <nav>
     </nav>
   )
 }
