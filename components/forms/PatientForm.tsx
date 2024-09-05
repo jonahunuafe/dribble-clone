46de715c986add3272e62570f86e05499911b4c4
Jonah Unuafe <fejisfejiro@gmail.com>
2024-09-05 13:37:27 +0100

    added custom form field

--- components/forms/PatientForm.tsx
+++ components/forms/PatientForm.tsx
@@ -5,15 +5,13 @@ import { useForm } from "react-hook-form"
 import { z } from "zod"
 import { Button } from "@/components/ui/button"
 import {
-  Form,
-  FormControl,
-  FormDescription,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
+  Form
 } from "@/components/ui/form"
-import { Input } from "@/components/ui/input"
+import CustomFormField from "../CustomFormField"
+
+export enum FormFieldType {
+  
+}
 
 const formSchema = z.object({
   username: z.string().min(2, {
@@ -39,23 +37,17 @@ const PatientForm = () => {
 
   return (
     <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-        <FormField
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6 flex-1">
+        <section className="mb-12 space-y-4">
+          <h1 className="header">Hi there 👋</h1>
+          <p className="text-dark-700">Schedule your first appointment.</p>
+        </section>
+        
+        <CustomFormField
+          fieldType={} 
           control={form.control}
-          name="username"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Username</FormLabel>
-              <FormControl>
-                <Input placeholder="shadcn" {...field} />
-              </FormControl>
-              <FormDescription>
-                This is your public display name.
-              </FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
         />
+
         <Button type="submit">Submit</Button>
       </form>
     </Form>
