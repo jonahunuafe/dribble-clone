3447dc9826e63c00952d69d1ec84e1e2d49a85fa
Jonah Unuafe <fejisfejiro@gmail.com>
2024-09-06 16:22:51 +0100

    installed node-appwrite inorder to configure our appwrite sdk

--- lib/appwrite.config.ts
+++ lib/appwrite.config.ts
@@ -1,4 +1,6 @@
-const {
+import * as sdk from 'node-appwrite'
+
+export const {
   PROJECT_ID, 
   API_KEY,DATABASE_ID, 
   PATIENT_COLLECTION_ID, 
@@ -7,3 +9,16 @@ const {
   NEXT_PUBLIC_BUCKET_ID: BUCKET_ID,
   NEXT_PUBLIC_BUCKET_ID: ENDPOINT,
 } = process.env;
+
+const client = new sdk.Client()
+
+// The exclamation mark means that we know that it exist.
+client
+  .setEndpoint(ENDPOINT!)
+  .setProject(PROJECT_ID!)
+  .setKey(API_KEY!)
+
+export const databases = new sdk.Databases(client)
+export const storage = new sdk.Storage(client)
+export const messaging = new sdk.Messaging(client)
+export const users = new sdk.Users(client)
