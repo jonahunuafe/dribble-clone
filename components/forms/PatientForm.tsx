c1e91603e94164077ae3e1cea14e5916d7820648
Jonah Unuafe <fejisfejiro@gmail.com>
2024-09-07 09:49:36 +0100

    implemented functionality to create a new user

--- components/forms/PatientForm.tsx
+++ components/forms/PatientForm.tsx
@@ -9,6 +9,7 @@ import SubmitButton from "../SubmitButton"
 import { useState } from "react"
 import { UserFormValidation } from "@/lib/validation"
 import { useRouter } from "next/navigation"
+import { createUser } from "@/lib/actions/patient.action"
 
 export enum FormFieldType {
   INPUT = "input",
@@ -41,11 +42,11 @@ const PatientForm = () => {
     setIsLoading(true);
 
     try {
-      // const userData = { name, email, phone };
+      const userData = { name, email, phone };
 
-      // const user = await createUser(userData)
+      const user = await createUser(userData)
 
-      // if(user) router.push(`/patients/${user.$id}/register`)
+      if(user) router.push(`/patients/${user.$id}/register`)
     } catch(error) {
       console.log(error)
     }
--- lib/appwrite.config.ts
+++ lib/appwrite.config.ts
@@ -2,12 +2,13 @@ import * as sdk from 'node-appwrite'
 
 export const {
   PROJECT_ID, 
-  API_KEY,DATABASE_ID, 
+  API_KEY,
+  DATABASE_ID, 
   PATIENT_COLLECTION_ID, 
   DOCTOR_COLLECTION_ID, 
   APPOINTMENT_COLLECTION_ID, 
   NEXT_PUBLIC_BUCKET_ID: BUCKET_ID,
-  NEXT_PUBLIC_BUCKET_ID: ENDPOINT,
+  APPWRITE_ENDPOINT: ENDPOINT,
 } = process.env;
 
 const client = new sdk.Client()
