74ac3e1ac5851218a8f3ad369cd35b87aa4ee788
Jonah Unuafe <fejisfejiro@gmail.com>
2024-09-09 16:15:02 +0100

    created fields for address and occupation on registration form

--- components/forms/RegisterForm.tsx
+++ components/forms/RegisterForm.tsx
@@ -125,8 +125,28 @@ const RegisterForm = ({ user }: {user: User}) => {
           />
         </div>
 
-        <div className="flex flex-col gap-6 xl:flex-row">
+        {/* <section className="space-y-6">
+          <div className="mb-9 space-y-1">
+            <h2 className="sub-header">Medical Infomaton</h2>
+          </div>
+        </section> */}
 
+        <div className="flex flex-col gap-6 xl:flex-row">
+          <CustomFormField
+              fieldType={FormFieldType.INPUT} 
+              control={form.control}
+              name="address"
+              label="Address"
+              placeholder="24th Street, Abuja"
+            />
+
+            <CustomFormField
+              fieldType={FormFieldType.PHONE_INPUT} 
+              control={form.control}
+              name="occupation"
+              label="Occupation"
+              placeholder="Software Engineer"
+            />
         </div>
 
         <div className="flex flex-col gap-6 xl:flex-row">
