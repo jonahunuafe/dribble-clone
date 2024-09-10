c934f9ad036419d51d2d9747b6ec2efcedd5a561
Jonah Unuafe <fejisfejiro@gmail.com>
2024-09-10 17:25:59 +0100

    passed in required props to fileuploader component inside of registration form

--- components/forms/RegisterForm.tsx
+++ components/forms/RegisterForm.tsx
@@ -282,7 +282,10 @@ const RegisterForm = ({ user }: {user: User}) => {
           label="Scanned copy of identification document"
           renderSkeleton={(field) => (
             <FormControl>
-              <FileUploader />
+              <FileUploader 
+                files={field.value}
+                onChange={field.onChange}
+              />
             </FormControl>
           )}
         />
