d48840b5a3299c130cf039633e7add9a1f68072e
Jonah Unuafe <fejisfejiro@gmail.com>
2024-09-12 14:52:15 +0100

    modified styling of copyright text

--- app/patients/[userId]/register/page.tsx
+++ app/patients/[userId]/register/page.tsx
@@ -20,15 +20,9 @@ const Register = async({ params : { userId } }: SearchParamProps) => {
           />
 
           <RegisterForm user={user} />
-
-          <div className='text-14-regular mt-20 flex justify-between'>
-            <p className='justify-items-end text-dark-600 xl:text-left'>
-              &copy; 2024 CarePulse
-            </p>
-            <Link href='/?admin=true' className='text-green-500'>
-              Admin
-            </Link>
-          </div>
+          <p className='copyright py-12'>
+            &copy; 2024 CarePulse
+          </p>
         </div>
       </section>
 
