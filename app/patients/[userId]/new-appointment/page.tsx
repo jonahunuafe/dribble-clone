b8434ef25bd9cde7948b95c8fff352cc88c5111e
Jonah Unuafe <fejisfejiro@gmail.com>
2024-09-16 08:35:51 +0100

    refactor newappointment page

--- app/patients/[userId]/new-appointment/page.tsx
+++ app/patients/[userId]/new-appointment/page.tsx
@@ -6,10 +6,8 @@ import React from 'react'
 const NewAppointment = () => {
   return (
     <div className='flex h-screen max-h-screen'>
-      {/* TODO: OTP verification | passkey model */}
-
       <section className="remove-scrollbar container my-auto">
-        <div className='sub-container max-w-[496px]'>
+        <div className='sub-container max-w-[860px] flex-1 justify-between'>
           <Image 
             src='/assets/icons/logo-full.svg'
             height={1000}
@@ -18,25 +16,21 @@ const NewAppointment = () => {
             className='mb-12 h-10 w-fit'
           />
 
-          <PatientForm />
+          {/* <PatientForm /> */}
 
-          <div className='text-14-regular mt-20 flex justify-between'>
-            <p className='justify-items-end text-dark-600 xl:text-left'>
-              &copy; 2024 CarePulse
-            </p>
-            <Link href='/?admin=true' className='text-green-500'>
-              Admin
-            </Link>
-          </div>
+          <p className='justify-items-end text-dark-600 xl:text-left'>
+            &copy; 2024 CarePulse
+          </p>
+          
         </div>
       </section>
 
       <Image
-        src='/assets/images/onboarding-img.png'
+        src='/assets/images/appointment-img.png'
         height={1000}
         width={1000}
-        alt='patient' 
-        className='side-img max-w-[50%]'
+        alt='appointment' 
+        className='side-img max-w-[390px] bg-bottom'
       />
     </div>
   )
