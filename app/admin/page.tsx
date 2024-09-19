353eeae1ac9c96136416e0c2843b39d61ea85684
Jonah Unuafe <fejisfejiro@gmail.com>
2024-09-19 08:36:14 +0100

    created StatCard component

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,3 +1,4 @@
+import StatCard from '@/components/StatCard'
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
@@ -24,7 +25,26 @@ const Admin = () => {
           <p className="text-dark-700">Start the day with managing new appointments</p>
         </section>
 
-        <section className="admin-stat"></section>
+        <section className="admin-stat">
+          <StatCard 
+            type="appointments"
+            count={5}
+            label="Scheduled appointments"
+            icon="/assets/icons/appointment.svg"
+          />
+          <StatCard 
+            type="pending"
+            count={10}
+            label="Pending appointments"
+            icon="/assets/icons/pending.svg"
+          />
+          <StatCard 
+            type="cancelled"
+            count={2}
+            label="Cancelled appointments"
+            icon="/assets/icons/cancelled.svg"
+          />
+        </section>
       </main>
     </div>
   )
