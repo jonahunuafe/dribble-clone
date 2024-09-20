097df9444e48c828e068045fd3991fae88f9ebbb
Jonah Unuafe <fejisfejiro@gmail.com>
2024-09-20 08:40:46 +0100

    installed shadcn drop dowm menu

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,14 +1,28 @@
-import DataTable from '@/components/table/DataTable'
+import { DataTable } from '@/components/table/DataTable'
 import StatCard from '@/components/StatCard'
 import { getRecentAppointmentList } from '@/lib/actions/appointment.actions'
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
-import { columns } from '@/components/table/Columns'
+import { columns, Payment } from '@/components/table/Columns'
+
+async function getData(): Promise<Payment[]> {
+  // Fetch data from your API here.
+  return [
+    {
+      id: "728ed52f",
+      amount: 100,
+      status: "pending",
+      email: "m@example.com",
+    },
+    // ...
+  ]
+}
 
 const Admin = async() => {
   const appointments = await getRecentAppointmentList()
 
+  const data = await getData()
   return (
     <div className="mx-auto flex max-w-7xl flex-col space-y-14">
       <header className="admin-header">
@@ -51,9 +65,13 @@ const Admin = async() => {
           />
         </section>
 
-        <DataTable
+        {/* <DataTable
           columns={columns} 
           data={appointments.documents}
+        /> */}
+        <DataTable
+          columns={columns} 
+          data={data}
         />
       </main>
     </div>
