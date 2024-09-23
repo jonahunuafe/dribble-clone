bee7c9c8fd378745e1fd5006f657e5e05ba3c6b4
Jonah Unuafe <fejisfejiro@gmail.com>
2024-09-23 22:36:52 +0100

    installed sentry

--- app/patients/[userId]/new-appointment/page.tsx
+++ app/patients/[userId]/new-appointment/page.tsx
@@ -1,11 +1,14 @@
-import Link from 'next/link';
 import Image from 'next/image';
 import React from 'react'
 import AppointmentForm from '@/components/forms/AppointmentForm';
 import { getPatient } from '@/lib/actions/patient.action';
+import * as Sentry from "@sentry/nextjs"
 
 const NewAppointment = async({ params: {userId} }: SearchParamProps) => {
   const patient = await getPatient(userId)
+
+  Sentry.metrics.set("user_view_new-appointment", patient.name)
+
   return (
     <div className='flex h-screen max-h-screen'>
       <section className="remove-scrollbar container my-auto">
