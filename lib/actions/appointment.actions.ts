2f1f61d5d573004d044d137eabbfd76d32a79679
Jonah Unuafe <fejisfejiro@gmail.com>
2024-09-24 00:42:18 +0100

    added configuration to nextConfig file

--- lib/actions/appointment.actions.ts
+++ lib/actions/appointment.actions.ts
@@ -91,7 +91,7 @@ export const updateAppointment = async({ appointmentId, userId, appointment, typ
       const smsMessage = `
         Hi, it's CarePulse.
         ${type === "schedule" ? `Your appointment has been scheduled for ${formatDateTime
-          (appointment.schedule!)}` 
+          (appointment.schedule!).dateTime} with Dr. ${appointment.primaryPhysician}.` 
           : `We regret to inform you that your appointment has been cancelled for the following reason. 
           Reason: ${appointment.cancellationReason}`
         }
